0f3eadbc4259296d96410a6ad2055ac4a36c5a4c
Marek Šuppa <mrshu@users.noreply.github.com>
2022-01-29 01:22:05 +0100

    fix: Add missing commas
    * Add missing commas

--- samples/features/sql-big-data-cluster/deployment/offline/push-bdc-images-to-custom-private-repo.py
+++ samples/features/sql-big-data-cluster/deployment/offline/push-bdc-images-to-custom-private-repo.py
@@ -27,27 +27,27 @@ TARGET_DOCKER_PASSWORD = getpass.getpass("Provide Docker password for the target
 TARGET_DOCKER_TAG = input("Provide Docker tag for the images at the target: ") or "latest"
 
 images = ['mssql-app-service-proxy',
-          'mssql-control-watchdog'
-           'mssql-controller'
-           'mssql-dns'
-           'mssql-hadoop'
-           'mssql-mleap-serving-runtime'
-           'mssql-mlserver-py-runtime'
-           'mssql-mlserver-r-runtime'
-           'mssql-monitor-collectd'
-           'mssql-monitor-elasticsearch'
-           'mssql-monitor-fluentbit'
-           'mssql-monitor-grafana'
-           'mssql-monitor-influxdb'
-           'mssql-monitor-kibana'
-           'mssql-monitor-telegraf'
-           'mssql-security-knox'
-           'mssql-security-support'
-           'mssql-server-controller'
-           'mssql-server-data'
-           'mssql-ha-operator'
-           'mssql-ha-supervisor'
-           'mssql-service-proxy'
+          'mssql-control-watchdog',
+           'mssql-controller',
+           'mssql-dns',
+           'mssql-hadoop,'
+           'mssql-mleap-serving-runtime',
+           'mssql-mlserver-py-runtime',
+           'mssql-mlserver-r-runtime',
+           'mssql-monitor-collectd',
+           'mssql-monitor-elasticsearch',
+           'mssql-monitor-fluentbit',
+           'mssql-monitor-grafana',
+           'mssql-monitor-influxdb',
+           'mssql-monitor-kibana',
+           'mssql-monitor-telegraf',
+           'mssql-security-knox',
+           'mssql-security-support',
+           'mssql-server-controller',
+           'mssql-server-data',
+           'mssql-ha-operator',
+           'mssql-ha-supervisor',
+           'mssql-service-proxy',
            'mssql-ssis-app-runtime']
 
 # Use this only if your source is a private Docker registry
